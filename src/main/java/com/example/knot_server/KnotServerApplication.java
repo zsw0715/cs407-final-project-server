4dbba6627f77f4d1d40a5f501925bb84c821fe66
莫塔里小土豆 <shenweizhang0715@outlook.com>
2025-09-30 14:05:00 -0500

    init repo

---
+++ src/main/java/com/example/knot_server/KnotServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.knot_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnotServerApplication.class, args);
+	}
+
+}
